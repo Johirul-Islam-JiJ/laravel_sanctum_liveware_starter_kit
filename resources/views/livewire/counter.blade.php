5217118f106691b7a1dee4e1e4cc793ceb166dbf
Johirul-Islam-JiJ <johirul35-257@diu.edu.bd>
2023-12-20 19:15:08 +0600

    authentication setup

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,3 +1,7 @@
 <div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+    <h1>{{ $count }}</h1>
+
+    <button wire:click="increment">+</button>
+
+    <button wire:click="decrement">-</button>
 </div>
